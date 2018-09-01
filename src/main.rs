628d163f1b136688b05b0f14b3966d83b63f0c41
Josh Mcguigan <joshmcg88@gmail.com>
2018-08-31 22:31:28 -0700

    Moved turn logic into player

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,7 @@ mod player;
 use player::Player;
 
 mod cli;
-
 mod dice;
-use dice::Dice;
-
-fn jail_roll(jailed_player: &mut Player) -> bool {
-    println!("{} is in jail", jailed_player.name);
-    jailed_player.jail_count += 1;
-
-    let dice = Dice::roll();
-
-    if dice.is_doubles() {
-        println!("{} got out of jail!", jailed_player.name);
-        jailed_player.jail_count = 0;
-        return false;
-    }
-    if jailed_player.jail_count == 3 {
-        println!("You must pay $50 to get out now.");
-        jailed_player.jail_count = 0;
-        return false;
-    }
-    true
-}
 
 fn main() {
     let number_of_players = cli::get_number_of_players();
@@ -38,33 +17,7 @@ fn main() {
 
     'game: loop {
         for mut player in players.iter_mut() {
-            if player.in_jail {
-                cli::printnls(100);
-
-                if !jail_roll(player) {
-                    player.in_jail = false;
-                }
-                continue;
-            }
-
-            cli::printnls(100);
-
-            println!("{}, it's your turn.", player.name);
-
-            while Dice::roll().is_doubles() {
-                player.doubles_roll += 1;
-
-                match player.doubles_roll {
-                    1 => println!("You rolled doubles! Play again!"),
-                    2 => println!("Doubles! If you do it again, you'll go to jail!"),
-                    3 | _ => {
-                        player.in_jail = true;
-                        println!("Go to jail!");
-                    }
-                }
-            }
-
-            player.doubles_roll = 0;
+            player.take_turn();
 
             if !cli::press_enter() { break 'game; }
         }
--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,6 @@
+use dice::Dice;
+use cli;
+
 pub struct Player {
     pub name: String,
     pub doubles_roll: u8,
@@ -13,5 +16,54 @@ impl Player {
             jail_count: 0,
             in_jail: false,
         }
+    }
+
+    pub fn take_turn(&mut self) {
+        if self.in_jail {
+            cli::printnls(100);
+
+            if !self.jail_roll() {
+                self.in_jail = false;
+            }
+            return;
+        }
+
+        cli::printnls(100);
+
+        println!("{}, it's your turn.", self.name);
+
+        while Dice::roll().is_doubles() {
+            self.doubles_roll += 1;
+
+            match self.doubles_roll {
+                1 => println!("You rolled doubles! Play again!"),
+                2 => println!("Doubles! If you do it again, you'll go to jail!"),
+                3 | _ => {
+                    self.in_jail = true;
+                    println!("Go to jail!");
+                }
+            }
+        }
+
+        self.doubles_roll = 0;
+    }
+
+    fn jail_roll(self: &mut Player) -> bool {
+        println!("{} is in jail", self.name);
+        self.jail_count += 1;
+
+        let dice = Dice::roll();
+
+        if dice.is_doubles() {
+            println!("{} got out of jail!", self.name);
+            self.jail_count = 0;
+            return false;
+        }
+        if self.jail_count == 3 {
+            println!("You must pay $50 to get out now.");
+            self.jail_count = 0;
+            return false;
+        }
+        true
     }
 }
