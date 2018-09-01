32a747f51dedd47a94c926b5fd09ef16a0656d9c
Josh Mcguigan <joshmcg88@gmail.com>
2018-08-31 22:48:40 -0700

    Implemented suggestions from other commenters

--- src/cli.rs
+++ src/cli.rs
@@ -3,9 +3,7 @@ use std::io;
 pub fn get_number_of_players() -> usize {
     println!("How many players?");
     loop {
-        let mut number_of_players = String::new();
-        io::stdin().read_line(&mut number_of_players)
-            .expect("Failed to read stdin.");
+        let number_of_players = user_input();
 
         let number_of_players: usize = match number_of_players.trim().parse() {
             Ok(nop) => nop,
@@ -23,12 +21,8 @@ pub fn get_number_of_players() -> usize {
 }
 
 pub fn get_player_name(player_number: usize) -> String {
-    let mut name = String::new();
     println!("Enter the name for player number {}.", player_number + 1);
-    io::stdin().read_line(&mut name)
-        .expect("Failed to read stdin.");
-
-    name.trim().to_string()
+    user_input().trim().to_string()
 }
 
 pub fn printnls(newlines: u8) {
@@ -41,14 +35,15 @@ pub fn press_enter() -> bool {
     println!("Press ENTER or RETURN to continue.");
     println!("Press a key before ENTER or RETURN to quit.");
 
-    let mut quit_input = String::new();
-    io::stdin().read_line(&mut quit_input)
-        .expect("Failed to read line.");
+    let quit_input = user_input();
 
-    if quit_input.starts_with('\n') {
-        return true;
-    }
-        else {
-            return false;
-        }
+    quit_input.starts_with('\n')
+}
+
+fn user_input() -> String {
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input)
+        .expect("Failed to read stdin.");
+
+    user_input
 }
